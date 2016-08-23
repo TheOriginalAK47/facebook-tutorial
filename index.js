ffa2a2c87907f42ad7ea14cda3989dc278064a5f
Brandon McFarland <branmcf@gmail.com>
2016-08-23 16:40:22 -0500

    playing around with waking the bot

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text.toLowerCase()
-            if (text === "hi" || text === "hello") {
+            if (text != null) {
+                //=== "hi" || text === "hello"
                 sendTextMessage(sender, "Welcome to MapleChat! Text MENU to get started!")
             } else if (text === "menu") {
                 sendTextMessage(sender, menuMsg)
